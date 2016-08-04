dd531430475eb4d7be99ecfc30dfe473be49e83d
Vinicius Pacheco <vin175pacheco@gmail.com>
2016-08-04 18:47:15 -0300

    Removed unused css files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
 import App from './App';
-import './index.css';
 
 const store = configureStore()
 
